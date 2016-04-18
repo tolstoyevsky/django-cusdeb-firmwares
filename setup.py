45f1993b7ccaba4411c2a0de61ba6b34abc6cdaf
Evgeny Golyshev <grga-pitic@yandex.ru>
2016-04-18 17:49:13 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-cusdeb-firmwares',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache 2.0',
+    description='',
+    long_description=README,
+    url='',
+    author='Evgeny Golyshev',
+    author_email='eugulixes@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
